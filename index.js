2a5d5a925d7e3f0a26207f75348234af7008f46a
movarnell <michaelvarnell@icloud.com>
2024-10-02 20:23:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Create a deck class to build my deck.
+// - Needs 4 suits, and 13 facevalues.
+// - How do I loop over the suits, and facevalues to make each card.
+
+// Decided how cards should be structure
+
+// How do I shuffle?
+// How do I either split the deck or deal cards.
+
+
+
+let card = {
+    suit: "diamonds",
+    facevalue: "A",
+    value: i,
+}
+
+
+
+
+
+
+class Deck{
+    constructor(){
+        //build the deck
+        this.deck = []
+    }
+}
